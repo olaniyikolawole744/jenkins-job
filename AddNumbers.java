00357d877808bd2ac0e7f776b834c794454c7fb2
Jenkins <jenkins@ip-172-31-91-51.ec2.internal>
2025-08-28 02:08:18 +0000

    jenkins job

---
+++ AddNumbers.java
@@ -0,0 +1,13 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
